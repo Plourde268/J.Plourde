8db2059a1c2ace7272114fd9c0c2132d40ef0bc6
Plourde268 <jst268@outlook.com>
2015-06-07 22:32:56 -0500

    Reverse order: Words

---
+++ ConsoleApplicationtest/ConsoleApplicationtest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace FirstApp
+{
+
+    class Program
+    {
+        public static string ReverseString(string yourSentence)
+        {
+            string[] deeznuts = yourSentence.Split(' ');
+            Array.Reverse(deeznuts);
+            return string.Join(" ", deeznuts);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your favorite sentence:");
+            var yourSentence = Console.ReadLine();
+            Console.WriteLine(ReverseString(yourSentence));
+            Console.ReadLine();
+        }
+    }
+}
