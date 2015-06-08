3142293a22c202ac58fe88a2330111b8daea6a9e
Plourde268 <jst268@outlook.com>
2015-06-07 22:46:49 -0500

    Extention Method

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Everything is Awesome!";
+            string revMessage = message.Reverse();
+            Console.WriteLine(revMessage);
+            Console.ReadLine();
+
+            message.Reverse().WriteColor(ConsoleColor.DarkCyan, ConsoleColor.DarkMagenta);
+
+            Console.ReadLine();
+        }
+    
+    }
+   
+
+    public static class StringExtensions
+    {
+        public static string Reverse(this string stringToExtend)
+        {
+            char[] textArray = stringToExtend.ToCharArray();      
+            Array.Reverse(textArray);
+            return new String(textArray);
+        }
+
+        public static void WriteColor(this string stringToExtend, ConsoleColor backgroundColor, ConsoleColor foregroundColor)
+        {
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(stringToExtend);
+        }
+    }
+}
---
+++ RandomNewsWithArray/RandomNewsWithArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNewsWithArray
+{
+    class Program
+    {
+
+        
+
+        
+
+        static void Main()
+        {
+            
+            var headLines = new string[]{
+                "Patriots win the Superbowl", "Angular 2.0 is Out", "The end is near"
+            };
+            Random rnd = new Random();
+
+            Console.WriteLine(headLines[rnd.Next(0, headLines.Length)]);
+            Console.ReadLine();
+            
+            Main();
+            
+        }
+    }
+}
