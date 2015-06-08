afd2a9470dd74136ce3b533aa858991158207b4b
Plourde268 <jst268@outlook.com>
2015-06-08 00:37:02 -0500

    Shopping Cart

---
+++ ShoppingList/ShoppingList/Models/ShoppingListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingListViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IList<Product> Products { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
