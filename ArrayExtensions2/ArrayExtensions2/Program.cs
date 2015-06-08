8ced987270e0f7715d1203c072ef123d56acd9e3
Plourde268 <jst268@outlook.com>
2015-06-07 22:51:06 -0500

    Extention Meth. on Array

---
+++ ArrayExtensions2/ArrayExtensions2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExtensions2
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            string[] headlines = new string[]{
+                "Martians Attack!",
+                "Seahawks lose Superbowl!",
+                "Mets suck"
+            };
+
+
+            Console.WriteLine(headlines.GetRandom());
+            Console.ReadLine();
+            Main();
+        }
+    }
+
+        public static class StringExtensions
+        {
+            public static string GetRandom(this Array arrayToExtend)
+            {
+                Random rnd = new Random();
+                //string result = arrayToExtend[rnd.Next(0, arrayToExtend.Length)];     //works with only string[]
+                //return result;
+
+                int index = rnd.Next(arrayToExtend.Length);
+                return arrayToExtend.GetValue(index).ToString();
+            }
+        }
+
+    
+
+
+
+}
