84867aca29b6016483a0f0cddf113182ccfcf0ed
Plourde268 <jst268@outlook.com>
2015-06-07 23:42:14 -0500

    MVC C#

---
+++ SimpleMVCApp/SimpleMVCApp/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMVCApp.Models
+{
+    public class Repository
+    {
+        public IList<Movie> ListMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Title = "Star Wars", Director = "Lucas"},
+                new Movie { Title = "King Kong", Director = "Jackson"},
+                new Movie { Title = "Memento", Director = "Nolan"},
+            
+
+            };
+        }
+    }
+}
