419ff9fd26d8e1659cba6124aaca989a5ff8f2b3
Plourde268 <jst268@outlook.com>
2015-06-08 00:30:21 -0500

    Strongly Typed Views

---
+++ StronglyTyped/StronglyTyped/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using StronglyTyped.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTyped.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product{Name="Milk", Price= 2.99m },
+                new Product{Name="Eggs", Price= 2.49m },
+                new Product{Name="Cheese", Price= 9.25m },
+
+            };
+
+            IndexViewModel vm = new IndexViewModel()
+            {
+                Products = products
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ StronglyTyped/StronglyTyped/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTyped.Models
+{
+    public class IndexViewModel
+    {
+        public IList<Product> Products { get; set; }
+    }
+}
